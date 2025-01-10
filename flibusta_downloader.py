1be2107df4ec41f5cfde299ec3ed7ecd6749f43e
artemyv <artemyv@gmail.com>
2025-01-10 09:37:58 +0200

    Enhance story downloader to show parts modified since last time book was saved

--- flibusta_downloader.py
+++ flibusta_downloader.py
@@ -8,6 +8,7 @@ import tkinter as tk
 # Add the EpubMerge folder to the path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../EpubMerge/epubmerge'))) 
 from epubmerge import doMerge
+from datetime import datetime
 
 
 
@@ -63,17 +64,27 @@ class WebPageParser:
                     p_tag = annotation_tag.find_next('p')
                     if p_tag:
                         annotation = p_tag.get_text().strip()
+                if annotation:
+                    logging.info(f"Found annotation for part {index} : {annotation}")
                 # get text from all a tags with class "genre" within main_div
                 for a in main_div.find_all('a', class_='genre'):
                     if a.get_text() not in tags:
                         tags.append(a.get_text())
-            
-            logging.info(f"Found annotation for part {index} : {annotation}")
-            return annotation
+                # Find text contains "Добавлена: dd.mm.yyyy" and parse date - for example Добавлена: 04.09.2022
+                found = re.search(r'Добавлена: (\d{2}\.\d{2}\.\d{4})', main_div.get_text())
+                if found:
+                    modified_date_str = found.group(1)
+                    if modified_date_str:
+                        modified = datetime.strptime(modified_date_str, '%d.%m.%Y')
+                        if modified:
+                            logging.info(f"Found modified date for part {index}: {modified}")
+                
+            return annotation, modified
+        
         except requests.RequestException as e:
             logging.error(f"Error fetching the annotation: {e}")
             
-    def get_story_links(self):
+    def get_story_links(self, lastUpdated):
         if self.soup is None:
             logging.warning("No content has been parsed.")
             print("No content has been parsed. Please fetch the page first and parse it.")
@@ -91,8 +102,13 @@ class WebPageParser:
             if re.fullmatch(r'/b/\d+', a['href']):
                 text_before_a = a.previous_sibling.strip() if a.previous_sibling else ''
                 logging.debug(f"Found part: {text_before_a} name: {a.get_text()}, url: {a['href']}")
-                annotation = self.get_annotation(a['href'], text_before_a, tags)
-                links.append({'url': a['href'], 'text': a.get_text(), 'index': text_before_a, 'annotation': annotation})
+                annotation, modified_date = self.get_annotation(a['href'], text_before_a, tags)
+                if lastUpdated:
+                    modified = modified_date > lastUpdated
+                else:
+                    modified = True
+                    
+                links.append({'url': a['href'], 'text': a.get_text(), 'index': text_before_a, 'annotation': annotation, 'modified': modified})
         self.tags = tags
         return links
 
@@ -130,8 +146,13 @@ class WebPageParser:
         entries = {}
         for link in links:
             text = f"{link['index']} - {link['text']}"
+            if link['modified']:
+                text += " *"
             listbox.insert(tk.END, text)
             entries[text] = link
+
+        # Select all entries by default
+        listbox.select_set(0, tk.END)
 
         btn_up = tk.Button(root, text="Move Up", command=move_up)
         btn_up.pack(side=tk.LEFT)
@@ -218,16 +239,31 @@ class WebPageParser:
         except Exception as e:
             print(f"Error importing epubmerge: {e}")
 
-if len(sys.argv) != 2:
-    print("Usage: python flibusta_downloader.py <URL>")
+def parse_date(date_str):
+    try:
+        return datetime.strptime(date_str, '%d %b %Y')
+    except ValueError as e:
+        logging.error(f"Error parsing date: {e}")
+        return None
+
+if len(sys.argv) < 2:
+    print("Usage: python flibusta_downloader.py <URL> [LastUpdated]")
     sys.exit(1)
 
 url = sys.argv[1] # https://flibusta.is/s/65539
+if len(sys.argv) > 2:
+    lastUpdated=sys.argv[2]
 
+if lastUpdated:
+    lastUpdatedDate = parse_date(lastUpdated)
+    if lastUpdatedDate:
+        logging.info(f"Parsed last updated date: {lastUpdatedDate}")
+    else:
+        logging.warning("Failed to parse last updated date.")
 parser = WebPageParser(url)
 parser.fetch_page()
 parser.parse_page()
 
-links = parser.get_story_links()
+links = parser.get_story_links(lastUpdatedDate)
 parser.show_links_in_listbox(links)
 parser.handle_selected()
